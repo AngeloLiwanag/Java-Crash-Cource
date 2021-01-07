e097025884635c4e203d676925266d2c6900ee81
Angelo Liwanag <angeloliwanag75@gmail.com>
2021-01-06 22:41:44 -0800

    Testing Java Waters

--- javaFun/HelloWorld.java
+++ javaFun/HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld { // your file name is exactly the same as your class
+    public static void main(String[] args) { // entry point method
         System.out.println("Hello World");
     }
 }
