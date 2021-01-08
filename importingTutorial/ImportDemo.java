1eac87f7e2a2b79f90c59be42aa3ed0ecfc0d750
Angelo Liwanag <angeloliwanag75@gmail.com>
2021-01-07 21:45:02 -0800

    Learning how to use imports in Java

---
+++ importingTutorial/ImportDemo.java
@@ -0,0 +1,7 @@
+import java.util.Date; // imports the date class
+public class ImportDemo {
+    public String getCurrentDate() {
+        Date date = new Date();
+        return "Current date is: " + date;
+    }
+}
