9405d93f860248190d90b3b408192a3585be8b42
Angelo Liwanag <angeloliwanag75@gmail.com>
2021-01-07 22:58:58 -0800

    FizzBuzz

---
+++ fizzBuzz/Output.java
@@ -0,0 +1,8 @@
+public class Output {
+    public static void main(String[] args) {
+        int num = 3;
+        FizzBuzz get = new FizzBuzz();
+        String output = get.fizzBuzz(num);
+        System.out.println(output);
+    }
+}
